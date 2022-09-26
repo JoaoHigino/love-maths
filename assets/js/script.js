a42bf2e45e52b19aebba6e160ea8da169876353a
JoaoHigino <jhigino10@hotmail.com>
2022-09-26 07:41:33 +0000

    Add code to calculate two random numbers for the gae question

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,47 +1,48 @@
-
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", function () {
     let buttons = document.getElementsByTagName("button");
-    for (let button of buttons){
-        button.addEventListener("click", function(){
-        if(this.getAttribute("data-type") === "submit"){
-            alert("You clicked submit!");
-        } else{
-            let gameType = this.getAttribute("data-type");
-            alert('You clicked ${gameType}');
-        }
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "submit") {
+                alert("You clicked submit!");
+            } else {
+                let gameType = this.getAttribute("data-type");
+                alert('You clicked ${gameType}');
+            }
         })
     }
 
 })
 
 
-function runGame(){
-
-}
-function checkAnswer(){
-
+function runGame() {
+    let num1 = Math.floor(Math.random() * 25)+ 1;
+    let num2 = Math.floor(Math.random() * 25)+ 1;
 }
 
-function calculateCorrectAnswer(){
+function checkAnswer() {
 
 }
 
-function incrementScore(){
+function calculateCorrectAnswer() {
 
 }
 
-function incrementWrongAnswer(){
+function incrementScore() {
 
 }
 
-function displayAdditionQuestion(){
+function incrementWrongAnswer() {
 
 }
 
-function displaySubractQuestion(){
+function displayAdditionQuestion() {
 
 }
 
-function displayMultiplyQuestion(){
+function displaySubractQuestion() {
+
+}
+
+function displayMultiplyQuestion() {
 
 }
